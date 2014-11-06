fa696245dc957bf81fd1ee780bd68b1aeaa602de
Joel Sommers <jsommers@acm.org>
2014-11-05 18:03:41 -0800

    star topology example: add calls to set addresses on interfaes

--- examples/star_topology.py
+++ examples/star_topology.py
@@ -3,18 +3,21 @@ from switchyard.lib.address import EthAddr, IPAddr
 from switchyard.lib.topo.util import *
 from switchyard.lib.topo.topobuild import *
 
-t = Topology()
+t = Topology(auto_macs=True,name="Example star topology")
 t.addRouter('router')
 for i in range(3):
     switchname = 'switch{}'.format(i+1)
     t.addSwitch(switchname)
     t.addLink('router', switchname, '10Mb/s', '100 msec')
+
+    rif,swif = t.getLinkInterfaces('router', switchname)
+    t.setInterfaceAddresses('router',rif,ip="192.168.{}.1".format(i+1),netmask="255.255.255.0")
     for j in range(3):
         hostname = 'host{}_{}'.format(i+1,j+1)
         t.addHost(hostname)
         t.addLink(switchname, hostname, '1Gb/s', '0.001')
 
-#        h1ifname,r1ifname = t.getLinkInterfaces('h1','r1')
-#        t.setInterfaceAddresses('h1',h1ifname,ip="10.0.1.1",netmask="255.255.0.0",mac="11:22:33:44:55:66") 
+        hif,swif = t.getLinkInterfaces(hostname, switchname)
+        t.setInterfaceAddresses(hostname,hif,ip="192.168.{}.{}".format(i+1,j+1),netmask="255.255.255.0")
 
 save_to_file(t, 'examples/star_topo.json')
