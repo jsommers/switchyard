2521d4d530191f707add38e321b800f56c0b601e
Joel Sommers <jsommers@acm.org>
2014-11-07 13:52:03 -0800

    Add code coverage to test run script

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
+
+coverage erase
 for f in tests/*.py
 do
-    python3 $f
+    # python3 $f
+    coverage run --include 'switchyard/*' -a $f
 done
+
+coverage report --include 'switchyard/*'
