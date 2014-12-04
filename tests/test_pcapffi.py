253b78ca9ea7c04e1f37b7a385ecfb6a13d6aa5c
Joel Sommers <jsommers@acm.org>
2014-12-03 22:00:05 -0500

    Add tests for pcapffi library

---
+++ tests/test_pcapffi.py
@@ -0,0 +1,41 @@
+import unittest 
+import os
+
+from switchyard.lib.pcapffi import *
+from switchyard.lib.packet import *
+
+class PcapFfiTests(unittest.TestCase):
+    def testWriteRead(self):
+        dump = PcapDumper("testXX.pcap")
+        pkt = Ethernet() + IPv6() + ICMPv6()
+        pkt[0].ethertype = EtherType.IPv6
+        pkt[1].nextheader = IPProtocol.ICMPv6
+        with self.assertRaises(PcapException):
+            dump.write_packet(pkt)
+        dump.write_packet(pkt.to_bytes())
+        dump.close()
+
+        reader = PcapReader("testXX.pcap")
+        count = 0
+        while True:
+            p = reader.recv_packet()
+            if not p:
+                break
+            count += 1
+            if count == 1:
+                rpkt = Packet(raw=p.raw)
+                self.assertEqual(pkt, rpkt)
+        reader.close()
+        self.assertEqual(count, 1)
+        os.unlink("testXX.pcap")
+
+
+    def testLive(self):
+        for d in pcap_devices():
+            if not d.isloop:
+                with self.assertRaises(PcapException):
+                    PcapLiveDevice(d.name.decode('ascii'))
+
+
+if __name__ == '__main__':
+    unittest.main()
