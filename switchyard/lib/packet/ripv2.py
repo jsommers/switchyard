c673216c9f5d20a776b263fdfbd8eb00fdcca6e0
Joel Sommers <jsommers@acm.org>
2014-12-10 20:08:36 -0500

    Basic tests for RIPv2 header completed

--- switchyard/lib/packet/ripv2.py
+++ switchyard/lib/packet/ripv2.py
@@ -1,9 +1,9 @@
 import struct
 from enum import Enum
+from ipaddress import IPv4Network
 
-from switchyard.lib.packet.packet import PacketHeaderBase,Packet
+from switchyard.lib.packet.packet import PacketHeaderBase,Packet,RawPacketContents
 from switchyard.lib.address import SpecialIPv4Addr, IPv4Address
-
 
 '''
 References:
@@ -14,40 +14,76 @@ class RIPCommand(Enum):
     Request = 1
     Reply = 2
 
-class RIPRouteEntry(PacketHeaderBase):
-    __slots__ = ('_family','_tag','_addr','_mask','_nexthop','_metric')
+class RIPRouteEntry(object):
+    __slots__ = ('_family','_tag','_addr','_nexthop','_metric')
     _PACKFMT = '!HHIIII'
     _MINLEN = struct.calcsize(_PACKFMT)
 
-    def __init__(self):
-        self.family = 2
-        self.tag = 0
-        self.addr = SpecialIPv4Address.INADDR_ANY
-        self.mask = SpecialIPv4Address.INADDR_ANY
-        self.nexthop = SpecialIPv4Address.INADDR_ANY
-        self.metric = 16
+    def __init__(self, address=SpecialIPv4Addr.IP_ANY.value, mask='255.255.255.255', nexthop='255.255.255.255', metric=16, tag=0):
+        self._family = 2
+        self._tag = tag
+        self._addr = IPv4Network("{}/{}".format(address,mask), strict=False)
+        self._nexthop = IPv4Address(nexthop)
+        self._metric = int(metric)
 
-    def size(self):
+    @staticmethod
+    def size():
         return RIPRouteEntry._MINLEN
 
+    def __str__(self):
+        return "{} -> {} {}".format(self._addr, self._nexthop, self._metric)
+
     def to_bytes(self):
-        return struct.pack(RIPRouteEntry._PACKFMT, self.family, self.tag,
-                           int(self.addr), int(self.mask), int(self.nexthop),
+        return struct.pack(RIPRouteEntry._PACKFMT, self._family, self.tag,
+                           int(self._addr.network_address), int(self._addr.netmask), int(self.nexthop),
                            self.metric)
 
-    def from_bytes(self, raw):
-        pass
+    @staticmethod
+    def from_bytes(raw):
+        if len(raw) != RIPRouteEntry._MINLEN:
+            raise Exception("Wrong number of bytes to reconstruct RIP Route Entry")
+        fields = struct.unpack(RIPRouteEntry._PACKFMT, raw)            
+        entry = RIPRouteEntry()
+        entry._family = int(fields[0])
+        entry._tag = int(fields[1])
+        entry._addr = IPv4Network(fields[2], strict=False)
+        entry._addr.netmask = IPv4Address(fields[3])
+        entry._nexthop = IPv4Address(fields[4])
+        entry._metric = int(fields[5])
+        return entry
 
-    # FIXME properties
+    @property 
+    def tag(self):
+        return self._tag
+
+    @property 
+    def address(self):
+        return self._addr.network_address
+
+    @property 
+    def mask(self):
+        return self._addr.netmask
+
+    @property 
+    def nexthop(self):
+        return self._nexthop
+
+    @property 
+    def metric(self):
+        return self._metric
+
 
 class RIPv2(PacketHeaderBase):
     __slots__ = ('_command','_domain','_routes')
     _PACKFMT = '!BBH'
     _MINLEN = struct.calcsize(_PACKFMT)
 
-    def __init__(self):
+    def __init__(self, raw=None):
         self.command = RIPCommand.Request
         self.domain = 0
+        self._routes = []
+        if raw:
+            self.from_bytes(raw)
 
     def size(self):
         return len(self.to_bytes())
@@ -56,18 +92,61 @@ class RIPv2(PacketHeaderBase):
         '''
         Return packed byte representation of the UDP header.
         '''
-        hdr = struct.pack(RIPv2._PACKFMT, self.command, self.version, self.domain)
+        hdr = struct.pack(RIPv2._PACKFMT, self.command.value, self.version, self.domain)
         routes = b''.join([r.to_bytes() for r in self._routes])
         return hdr + routes
 
     def from_bytes(self, raw):
-        pass
+        if isinstance(raw, RawPacketContents):
+            raw = raw.to_bytes()
+        if len(raw) < RIPv2._MINLEN:
+            raise Exception("Not enough bytes to reconstruct RIPv2 header")
+        fields = struct.unpack(RIPv2._PACKFMT, raw[:RIPv2._MINLEN])
+        self.command = fields[0]
+        self.domain = fields[2]
+        remain = raw[RIPv2._MINLEN:]
+        esize = RIPRouteEntry.size()
+        numroutes = len(remain) // esize
+        extra = len(remain) % esize
+        if extra > 0:
+            log_warn("RIPv2 payload isn't of expected size (i.e., an integral number of route entries).  Continuing anyway.")
+        for i in range(numroutes):
+            self._routes.append(RIPRouteEntry.from_bytes(remain[(i*esize):((i+1)*esize)]))
 
     def __eq__(self, other):
-        pass
+        return self.to_bytes() == other.to_bytes()
 
     def __str__(self):
-        return self.__class__.__name__
+        rdata = ' ({} routes: {})'.format(len(self._routes), ', '.join([str(r) for r in self._routes]))
+        if self.command == RIPCommand.Request:
+            rdata = ''
+        return "{} {}{}".format(self.__class__.__name__, self.command.name, rdata)
+
+    def __len__(self):
+        return len(self._routes)
+
+    def __getitem__(self, index):
+        if not isinstance(index, int):
+            raise TypeError("Indexing in RIPv2 requires an int")
+        if index < 0:
+            index = len(self._routes) + index
+        if not 0 <= index < len(self._routes):
+            raise IndexError("Bad index in RIPv2 route entry access")
+        return self._routes[index]
+
+    def __setitem__(self, index, routeentry):
+        if not isinstance(routeentry, RIPRouteEntry):
+            raise ValueError("Value must be an RIPRouteEntry object")
+        if index < 0:
+            index = len(self._routes) + index
+        if not 0 <= index < len(self._routes):
+            raise IndexError("Index out of range")
+        self._routes[index] = routeentry
+
+    def append(self, routeentry):
+        if not isinstance(routeentry, RIPRouteEntry):
+            raise ValueError("Value must be an RIPRouteEntry object")
+        self._routes.append(routeentry)
 
     @property 
     def command(self):
--- tests/test_ripv2.py
+++ tests/test_ripv2.py
@@ -1,12 +1,41 @@
-from switchyard.lib.packet import *
-from switchyard.lib.address import EthAddr, IPAddr
 import unittest
 
+from switchyard.lib.packet import *
+from switchyard.lib.address import EthAddr, IPAddr
+from switchyard.lib.pcapffi import PcapDumper
+
 class RIPv2PacketTests(unittest.TestCase):
-    def testCreateSerialize(self):
+    def testRequest(self):
         p = Ethernet() + IPv4() + UDP() + RIPv2()
+        p[0].src = '00:11:22:33:44:55'
+        p[0].dst = '55:44:33:22:11:00'
         p[1].protocol = IPProtocol.UDP
-        print (p) 
+        p[1].srcip = '192.168.100.42'
+        p[1].dstip = '192.168.100.255'
+        p[2].srcport = 5000
+        p[2].dstport = 520
+        xraw = p.to_bytes()
+        pkt = Packet(raw=xraw)
+        pkt[-1] = RIPv2(pkt[-1])
+        self.assertEqual(pkt, p)
+
+    def testReply(self):
+        p = Ethernet() + IPv4() + UDP() + RIPv2()
+        p[0].src = '00:11:22:33:44:55'
+        p[0].dst = '55:44:33:22:11:00'
+        p[1].protocol = IPProtocol.UDP
+        p[1].srcip = '192.168.100.42'
+        p[1].dstip = '192.168.100.255'
+        p[2].srcport = 5000
+        p[2].dstport = 520
+        p[3].command = RIPCommand.Reply
+        p[3].append(RIPRouteEntry('192.168.200.0','255.255.255.0','192.168.200.254',4))
+        p[3].append(RIPRouteEntry('192.168.100.0','255.255.252.0','192.168.100.254',3))
+        xraw = p.to_bytes()
+        pkt = Packet(raw=xraw)
+        pkt[-1] = RIPv2(pkt[-1])
+        self.assertEqual(pkt, p)
+
 
 if __name__ == '__main__':
     unittest.main()
