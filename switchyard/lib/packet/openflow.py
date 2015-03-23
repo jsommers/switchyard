5b3ca66ddf71a37a58f8ede7b437463088386a32
Joel Sommers <jsommers@colgate.edu>
2015-03-23 16:04:28 -0400

    Working on features request messages

--- switchyard/lib/packet/openflow.py
+++ switchyard/lib/packet/openflow.py
@@ -1,6 +1,9 @@
 from switchyard.lib.packet.packet import PacketHeaderBase
+from switchyard.lib.address import EthAddr
 from enum import Enum
 import struct
+
+OpenflowTypeClasses = {}
 
 class OpenflowType(Enum):
     Hello = 0
@@ -38,6 +41,7 @@ class OpenflowPort(Enum):
     NoPort = 0xffff # Can't use None!
 
 class OpenflowPortState(Enum):
+    NoState = 0
     LinkDown = 1 << 0
     StpListen = 0 << 8
     StpLearn = 1 << 8
@@ -46,6 +50,7 @@ class OpenflowPortState(Enum):
     StpMask = 3 << 8
 
 class OpenflowPortConfig(Enum):
+    NoConfig = 0
     Down = 1 << 0
     NoStp = 1 << 1
     NoRecv = 1 << 2
@@ -55,6 +60,7 @@ class OpenflowPortConfig(Enum):
     NoPacketIn = 1 << 6
 
 class OpenflowPortFeatures(Enum):
+    NoFeatures = 0
     e10Mb_Half = 1 << 0
     e10Mb_Full = 1 << 1
     e100Mb_Half = 1 << 2
@@ -68,17 +74,56 @@ class OpenflowPortFeatures(Enum):
     Pause = 1 << 10
     PauseAsym = 1 << 11
 
-class OpenflowStruct(PacketHeaderBase):
-    pass
+class _OpenflowStruct(PacketHeaderBase):
+    def __init__(self):
+        PacketHeaderBase.__init__(self)
 
-class OpenflowPhysicalPort(OpenflowStruct):
+    def __eq__(self, other):
+        return self.to_bytes() == other.to_bytes()
+
+    def next_header_class(self):
+        pass
+
+    def pre_serialize(self):
+        pass
+
+    def size(self):
+        return len(self.to_bytes())
+
+class OpenflowPhysicalPort(_OpenflowStruct):
     __slots__ = ['_portnum','_hwaddr','_name','_config',
                  '_state','_curr','_advertised','_supported','_peer']
     _PACKFMT = '!H6s16sIIIIII'
     _MINLEN = struct.calcsize(_PACKFMT)
-    
 
-class OpenflowPacketQueue(OpenflowStruct):
+    def __init__(self):
+        _OpenflowStruct.__init__(self)
+        self._portnum = 0
+        self._hwaddr = EthAddr()
+        self._name = ''
+        self._config = OpenflowPortConfig.NoConfig
+        self._state = OpenflowPortState.NoState
+        self._curr = 0
+        self._advertised = 0
+        self._supported = 0
+        self._peer = 0
+
+    def to_bytes(self):
+        pass
+
+    def from_bytes(self, raw):
+        pass
+
+    @property
+    def portnum(self):
+        return self._portnum
+    
+    @portnum.setter
+    def portnum(self, value):
+        self._portnum = int(value)        
+
+
+class OpenflowPacketQueue(_OpenflowStruct):
     __slots__ = ['_queue_id', '_properties']
     _PACKFMT = '!IHxx'
     _MINLEN = struct.calcsize(_PACKFMT)
@@ -87,14 +132,12 @@ class OpenflowQueuePropertyTypes(Enum):
     NoProperty = 0
     MinRate = 1
 
-class OpenFlowQueueMinRateProperty(OpenflowStruct):
+class OpenFlowQueueMinRateProperty(_OpenflowStruct):
     __slots__ = ['_rate']
     _PACKFMT = '!HH4xH6x'
     _MINLEN = struct.calcsize(_PACKFMT)
 
-OpenflowTypeClasses = {}
-
-class OpenflowMatch(OpenflowStruct):
+class OpenflowMatch(_OpenflowStruct):
     __slots__ = ['wildcards','_in_port','_dl_src','_dl_dst',
                  '_dl_vlan','_dl_vlan_pcp','_dl_type',
                  '_nw_tos','_nw_proto','_nw_src','_nw_dst',
@@ -141,7 +184,7 @@ class OpenflowActionType(Enum):
     Enqueue = 11
     Vendor = 0xffff
 
-class OpenflowHeader(object):
+class OpenflowHeader(_OpenflowStruct):
     '''
     Standard 8 byte header for all Openflow packets.
     This is a mostly-internal class used by the various
@@ -155,7 +198,6 @@ class OpenflowHeader(object):
         '''
         ofp_header struct from Openflow v1.0.0 spec.
         '''
-        PacketHeaderBase.__init__(self)
         self._version = 0x01
         self._type = xtype
         self._length = OpenflowHeader._MINLEN
@@ -190,12 +232,6 @@ class OpenflowHeader(object):
     def to_bytes(self):
         return struct.pack(OpenflowHeader._PACKFMT, self._version, self._type.value, self._length, self._xid)
 
-    def __eq__(self, other):
-        return self.version == other.version and \
-            self.type == other.type and \
-            self.length == other.length and \
-            self.xid == other.xid
-
     def size(self):
         return OpenflowHeader._MINLEN
 
@@ -225,7 +261,7 @@ class OpenflowMessage(PacketHeaderBase):
         return self._header.from_bytes(raw)
 
     def __eq__(self, other):
-        return self._header == other._header
+        return self.to_bytes() == other.to_bytes()
 
     def size(self):
         return self._header.size()
@@ -240,6 +276,42 @@ class OpenflowHello(OpenflowMessage):
     def __init__(self, xid=0):
         OpenflowMessage.__init__(self, OpenflowType.Hello, xid)
 
+class OpenflowError(OpenflowMessage):
+    def __init__(self, xid=0):
+        OpenflowMessage.__init__(self, OpenflowType.Error, xid)
+
+class OpenflowEchoRequest(OpenflowMessage):
+    __slots__ = ['_data']
+    def __init__(self, xid=0):
+        OpenflowMessage.__init__(self, OpenflowType.EchoRequest, xid)
+        self._data = b''
+
+    @property 
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, bytes):
+            raise ValueError("Data value must be bytes object")
+        self._data = value
+
+    def to_bytes(self):
+        return self._header + self.data
+
+    def from_bytes(self, raw):
+        data = self._header.from_bytes(raw)
+        self.data = data
+
+class OpenflowEchoReply(OpenflowEchoRequest):
+    def __init__(self, xid=0):
+        OpenflowEchoRequest(xid)
+        self._header.type = OpenflowType.EchoReply
+
+class OpenflowVendor(OpenflowMessage):
+    def __init__(self, xid=0):
+        OpenflowMessage.__init__(self, OpenflowType.Vendor, xid)
+
 class OpenflowSwitchFeaturesRequest(OpenflowMessage):
     def __init__(self, xid=0):
         OpenflowMessage.__init__(self, OpenflowType.FeaturesRequest, xid)
@@ -250,17 +322,41 @@ class OpenflowSwitchFeaturesReply(OpenflowMessage):
     '''
     __slots__ = ['_dpid','_nbuffers','_ntables','_capabilities',
                  '_actions', '_ports' ]
-    _PACKFMT = '!QIBxxxII'
+    _PACKFMT = '!8sIBxxxII'
     _MINLEN = struct.calcsize(_PACKFMT)
 
     def __init__(self, xid=0):
         OpenflowMessage.__init__(self, OpenflowType.FeaturesReply, xid)
-        self._dpid = 0
-        self._nbuffers = 0
-        self._ntables = 0
-        self._capabilities = 0
-        self._actions = 0
+        self.dpid = b'\x00' * 8
+        self.nbuffers = 0
+        self.ntables = 1
+        self._capabilities = OpenflowPortFeatures.NoFeatures.value
+        self._actions = OpenflowPortFeatures.NoFeatures.value
         self._ports = []
+
+    def to_bytes(self):
+        header = self._header.to_bytes()
+        remainder = struct.pack(OpenflowSwitchFeaturesReply._PACKFMT,
+            self._dpid, self._nbuffers, self._ntables,
+            self._capabilities, self._actions)
+        rawpkt = header + remainder
+        for p in self._ports:
+            rawpkt += p.to_bytes()
+        return rawpkt
+
+    def from_bytes(self, raw):
+        remain = self._header.from_bytes(raw)
+        fields = struct.unpack(OpenflowSwitchFeaturesReply._PACKFMT, 
+                               raw[:OpenflowSwitchFeaturesReply._MINLEN])
+        self.dpid = fields[0]
+        self.nbuffers = fields[1]
+        self.ntables = fields[2]
+        self.capabilities = fields[3]
+        self.actions = fields[4]
+        # FIXME ports
+
+    def size(self):
+        return len(self.to_bytes())
 
     @property 
     def dpid(self):
@@ -268,14 +364,110 @@ class OpenflowSwitchFeaturesReply(OpenflowMessage):
     
     @dpid.setter
     def dpid(self, value):
-        pass
+        if not isinstance(value, bytes):
+            raise ValueError("Setting dpid directly requires bytes object")
+        if len(value) > 8:
+            raise ValueError("dpid can only be 8 bytes")
+        # pad high-order bytes if we don't get 8 bytes as the value
+        self._dpid = b'\x00' * (8-len(value)) + value
+
+    @property 
+    def dpid_low48(self):
+        return self._dpid[2:8]
+
+    @dpid_low48.setter
+    def dpid_low48(self, value):
+        if isinstance(value, EthAddr):
+            self._dpid = self.dpid_high16 + value.raw
+        elif isinstance(value, bytes):
+            if len(value) != 6:
+                raise ValueError("Exactly 48 bits (6 bytes) must be given")
+            self._dpid = self.dpid_high16 + value.raw
+        else:
+            raise ValueError("Setting low-order 48 bits of dpid must be done with EthAddr or bytes")
+
+    @property 
+    def dpid_high16(self):
+        return self._dpid[0:2]
+
+    @dpid_high16.setter
+    def dpid_high16(self, value):
+        if isinstance(value, bytes):
+            if len(value) != 2:
+                raise ValueError("Exactly 16 bits (2 bytes) must be given")
+            self._dpid = value + self.dpid_low48
+        else:
+            raise ValueError("Setting high-order 16 bits of dpid must be done with bytes")
 
     @property 
     def nbuffers(self):
         return self._nbuffers
 
-    @dpid.setter
-    def dpid(self, value):
-        pass
+    @nbuffers.setter
+    def nbuffers(self, value):
+        value = int(value)
+        if 0 <= value < 2**32:
+            self._nbuffers = value
+        else:
+            raise ValueError("Number of buffers must be zero or greater.")
 
+    @property 
+    def ntables(self):
+        return self._ntables
 
+    @ntables.setter
+    def ntables(self, value):
+        value = int(value)
+        if 0 < value < 256:
+            self._ntables = value
+        else:
+            raise ValueError("Number of tables must be 1-255.")
+
+    @property 
+    def capabilities(self):
+        return self._capabilities
+
+    @capabilities.setter
+    def capabilities(self, value):
+        if isinstance(value, int): 
+            value = OpenflowPortFeatures(value)
+        if not isinstance(value, OpenflowPortFeatures):
+            raise ValueError("Set value must be of type OpenflowPortFeatures")
+        self._capabilities = self._capabilities | value.value
+
+    @property 
+    def actions(self):
+        return self._actions
+
+    @actions.setter
+    def actions(self, value):
+        if isinstance(value, int): 
+            value = OpenflowPortFeatures(value)
+        if not isinstance(value, OpenflowPortFeatures):
+            raise ValueError("Set value must be of type OpenflowPortFeatures")
+        self._actions = self._actions | value.value
+
+OpenflowTypeClasses = {
+    OpenflowType.Hello: OpenflowHello,
+    OpenflowType.Error: OpenflowError,
+    OpenflowType.EchoRequest: OpenflowEchoRequest,
+    OpenflowType.EchoReply: OpenflowEchoReply,
+    OpenflowType.Vendor: OpenflowVendor,
+    OpenflowType.FeaturesRequest: OpenflowSwitchFeaturesRequest,
+    OpenflowType.FeaturesReply: OpenflowSwitchFeaturesReply,
+    # OpenflowType.GetConfigRequest: OpenflowGetConfigRequest,
+    # OpenflowType.GetConfigReply: OpenflowGetConfigReply,
+    # OpenflowType.SetConfig: OpenflowSetConfig,
+    # OpenflowType.PacketIn: OpenflowPacketIn,
+    # OpenflowType.FlowRemoved: OpenflowFlowRemoved,
+    # OpenflowType.PortStatus: OpenflowPortStatus,
+    # OpenflowType.PacketOut: OpenflowPacketOut,
+    # OpenflowType.FlowMod: OpenflowFlowMod,
+    # OpenflowType.PortMod: OpenflowPortMod,
+    # OpenflowType.StatsRequest: OpenflowStatsRequest,
+    # OpenflowType.StatsReply: OpenflowStatsReply,
+    # OpenflowType.BarrierRequest: OpenflowBarrierRequest,
+    # OpenflowType.BarrierReply: OpenflowBarrierReply,
+    # OpenflowType.QueueGetConfigRequest: OpenflowQueueGetConfigRequest,
+    # OpenflowType.QueueGetConfigReply: OpenflowQueueGetConfigReply,
+}
--- tests/test_openflow.py
+++ tests/test_openflow.py
@@ -9,6 +9,11 @@ class OpenflowPacketTests(unittest.TestCase):
         self.assertEqual(hello.to_bytes(), b'\x01\x00\x00\x08\x00\x00\x00\x00')
         hello.header.xid = 42
         self.assertEqual(hello.to_bytes(), b'\x01\x00\x00\x08\x00\x00\x00\x2a')
+        bval = hello.to_bytes()
+
+        hello2 = OpenflowHello()
+        hello2.from_bytes(bval)
+        self.assertEqual(hello, hello2)
        
     def testSwitchFeatureRequest(self):
         featuresreq = OpenflowSwitchFeaturesRequest()
@@ -16,7 +21,15 @@ class OpenflowPacketTests(unittest.TestCase):
 
     def testSwitchFeatureReply(self):
         featuresreply = OpenflowSwitchFeaturesReply()
+        featuresreply.dpid_low48 = EthAddr("00:01:02:03:04:05")
+        featuresreply.dpid_high16 = b'\xab\xcd'
         self.assertEqual(featuresreply.to_bytes(), b'\x01\x06\x00\x08\x00\x00\x00\x00')
+
+    def testEchoRequest(self):
+        echoreq = OpenflowEchoRequest()        
+
+    def testEchoReply(self):
+        echorepl = OpenflowEchoReply()
 
 
 if __name__ == '__main__':
