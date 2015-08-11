e40247166700a259d09e6136300a0588f9dd372e
Joel Sommers <jsommers@colgate.edu>
2015-08-11 13:57:09 -0400

    ofswitch: add start of external controller tests

---
+++ tests/test_ofswitch_ext.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+'''
+OF switch tests for use with an external controller.
+'''
+
+import sys
+from switchyard.lib.address import *
+from switchyard.lib.packet import *
+from switchyard.lib.common import *
+from switchyard.lib.testing import *
+from switchyard.lib.openflow import *
+
+
+def mk_pkt(hwsrc, hwdst, ipsrc, ipdst, reply=False):
+    ether = Ethernet()
+    ether.src = EthAddr(hwsrc)
+    ether.dst = EthAddr(hwdst)
+    ether.ethertype = EtherType.IP
+
+    ippkt = IPv4()
+    ippkt.srcip = IPAddr(ipsrc)
+    ippkt.dstip = IPAddr(ipdst)
+    ippkt.protocol = IPProtocol.ICMP
+    ippkt.ttl = 32
+
+    icmppkt = ICMP()
+    if reply:
+        icmppkt.icmptype = ICMPType.EchoReply
+    else:
+        icmppkt.icmptype = ICMPType.EchoRequest
+    return ether + ippkt + icmppkt
+
+
+def ofswitch_tests():
+    s = Scenario("Openflow Switch Tests")
+    s.add_interface('eth0', '10:00:00:00:00:01')
+    s.add_interface('eth1', '10:00:00:00:00:02')
+    s.add_interface('eth2', '10:00:00:00:00:03')
+
+    # test case 1: a frame with broadcast destination should get sent out
+    # all ports except ingress
+    testpkt = mk_pkt(
+        "30:00:00:00:00:02", "ff:ff:ff:ff:ff:ff", "172.16.42.2", "255.255.255.255")
+    s.expect(PacketInputEvent("eth1", testpkt, display=Ethernet),
+             "An Ethernet frame with a broadcast destination address should arrive on eth1")
+    s.expect(PacketOutputEvent("eth0", testpkt, "eth2", testpkt, display=Ethernet),
+             "The Ethernet frame with a broadcast destination address should be forwarded out ports eth0 and eth2")
+
+    return s
+
+
+scenario = ofswitch_tests()
