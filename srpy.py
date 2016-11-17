1007a47d003930567816d3704a3eb4a4c2f10800
Joel Sommers <jsommers@colgate.edu>
2016-11-17 19:56:05 +1300

    Change separate -s/-t to just -t.  Fix firewall handling when in test mode.

--- srpy.py
+++ srpy.py
@@ -49,25 +49,51 @@ def start_app(appcode, firewall_setup):
         netobj.shutdown()
 
 if __name__ == '__main__':
-    progname = "srpy"
+    progname = "Switchyard"
     parser = argparse.ArgumentParser(prog=progname)
-    parser.add_argument("-t", "--test", help="Run {} in testing mode.".format(progname), dest="testmode", action="store_true", default=False)
-    parser.add_argument("-i", "--include", metavar='INCLUDE_INTF', help="Specify interface names to include/use for data plane traffic (default: all non-loopback interfaces).", dest="intf", action='append')
-    parser.add_argument("-x", "--exclude", metavar='EXCLUDE_INTF', help="Specify interface names to exclude in {}.  All other non-loopback interfaces will be used.".format(progname), dest="exclude", action='append')
-    parser.add_argument('usercode', metavar="YOURCODE", type=str, nargs='?', help='User switch/router code to execute.')
-    parser.add_argument("-c", "--compile", help="Compile scenario to binary format for distribution.", dest="compile", action="store_true", default=False)
-    parser.add_argument("-s", "--scenario", help="Specify scenario file to use in test mode.", dest="scenario", action="append")
-    parser.add_argument("--dryrun", help="Get everything ready to go, but don't actually do anything.", action='store_true', dest='dryrun', default=False)
-    parser.add_argument("-v", "--verbose", help="Turn on verbose output, including full packet dumps in test results.  Can be specified multiple times to increase verbosity.", dest="verbose", action="count", default=0)
-    parser.add_argument("-d", "--debug", help="Turn on debug logging output.", dest="debug", action="store_true", default=False)
-    parser.add_argument("--nopdb", help="Don't enter pdb on crash.", dest="nopdb", action="store_true", default=False)
-    parser.add_argument("-f", "--firewall", help="Specify host firewall rules (for real/live mode only)", dest="fwconfig", action="append")
-    parser.add_argument("-a", "--app", help="Specify application layer (socket-based) program to start (EXPERIMENTAL!)", dest="app", default=None)
-    parser.add_argument("-e", "--nohandle", help="Don't trap exceptions.  Use of this option is helpful if you want to use Switchyard with a different symbolic debugger than pdb", dest="nohandle", action="store_true", default=False)
+    parser.add_argument("-i", "--include", metavar='INCLUDE_INTF', 
+        help="Specify interface names to include/use for data plane traffic "
+             "(default: all non-loopback interfaces).", 
+        dest="intf", action='append')
+    parser.add_argument("-x", "--exclude", metavar='EXCLUDE_INTF', 
+        help="Specify interface names to exclude in {}.  "
+             "All other non-loopback interfaces will be used.".format(progname), 
+             dest="exclude", action='append')
+    parser.add_argument('usercode', metavar="YOURCODE", type=str, nargs='?', 
+        help='User switch/router code to execute.')
+    parser.add_argument("-c", "--compile", 
+        help="Compile test scenario to binary format for distribution.", 
+        dest="compile", action="append")
+    parser.add_argument("-t", "--test", 
+        help="Run {} in testing mode, using the given test "
+             "scenario file.".format(progname), 
+        dest="tests", action="append")
+    parser.add_argument("--dryrun", 
+        help="Get everything ready to go, but don't actually do anything.", 
+        action='store_true', dest='dryrun', default=False)
+    parser.add_argument("-v", "--verbose", 
+        help="Turn on verbose output, including full packet dumps in test "
+             "results. Can be specified multiple times to increase verbosity.",
+             dest="verbose", action="count", default=0)
+    parser.add_argument("-d", "--debug", help="Turn on debug logging output.", 
+        dest="debug", action="store_true", default=False)
+    parser.add_argument("--nopdb", help="Don't enter pdb on crash.", 
+        dest="nopdb", action="store_true", default=False)
+    parser.add_argument("-f", "--firewall", 
+        help="Specify host firewall rules (for real/live mode only)", 
+        dest="fwconfig", action="append")
+    parser.add_argument("-a", "--app", 
+        help="Specify application layer (socket-based) program to start", 
+        dest="app", default=None)
+    parser.add_argument("-e", "--nohandle", 
+        help="Don't trap exceptions.  Use of this option is helpful if you want"
+             " to use Switchyard with a different symbolic debugger than pdb", 
+             dest="nohandle", action="store_true", default=False)
     args = parser.parse_args()
 
     # assume test mode if the compile flag is set
-    if args.compile:
+    args.testmode = False
+    if args.compile or args.tests:
         args.testmode = True
 
     if args.verbose:
@@ -76,12 +102,13 @@ if __name__ == '__main__':
     setup_logging(args.debug)
 
     if args.usercode is None and not args.compile:
-        log_failure("You need to specify the name of your module to run as the last argument")
+        log_failure("You need to specify the name of your module to run "
+                    "as the last argument")
         sys.exit()
 
-    waiters = 1 # wait for app layer to init
-    if args.app and not args.testmode:
-        waiters += 1 # also wait for firewall to init if not in test
+    waiters = 1 
+    if args.app:
+        waiters += 1 
     barrier = Barrier(waiters)
 
     if args.app:
@@ -91,8 +118,12 @@ if __name__ == '__main__':
 
     if args.testmode:
         if args.usercode and args.compile:
-            log_info("You specified user code to run with compile flag, but I'm just doing compile.")
-        main_test(args.usercode, args.scenario, args)
+            log_info("You specified user code to run with compile flag, "
+                     "but I'm just doing compile.")
+        with Firewall([], args.fwconfig, testmode=True):
+            setup_ok = True
+            barrier.wait() 
+            main_test(args.usercode, args.tests, args)
     else:
         if sys.platform != 'win32' and os.geteuid() != 0:
             log_warn("You're running in real mode, but not as root.  "
@@ -110,9 +141,11 @@ if __name__ == '__main__':
 
         devlist = make_device_list(args.intf, args.exclude)
         if not devlist:
-            log_failure("There are no network interfaces I can use after processing include/exclude lists")
+            log_failure("There are no network interfaces I can use after "
+                        "processing include/exclude lists")
             alldevs = make_device_list([], [])
-            log_failure("Here are all the interfaces I see on your system: {}".format(', '.join(list(alldevs))))
+            log_failure("Here are all the interfaces I see on your system: "
+                        "{}".format(', '.join(list(alldevs))))
             barrier.wait()
             sys.exit()
 
